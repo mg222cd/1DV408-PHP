c66bc9890cb7e65b6d440d95e1c76e31d22fbcfd
Marike Grinde <mg222cd@student.lnu.se>
2014-10-02 10:45:16 +0200

    mer kod för UC 4.8

--- Labb4 - registrering/LoginApp/Model/User.php
+++ Labb4 - registrering/LoginApp/Model/User.php
@@ -1,8 +1,18 @@
 <?php
+require_once('./Model/UserList.php');
 
 class User{
 	private $userId;
 	private $username;
 	private $password;
 
+	public function __construct($userId, $username, $password){
+		$this->userId = $userId;
+		$this->username = $username;
+		$this->password = $password;
+	}
+
+	public function getName(){
+		return $this->username;
+	}
 }
--- Labb4 - registrering/LoginApp/Model/UserRepository.php
+++ Labb4 - registrering/LoginApp/Model/UserRepository.php
@@ -6,5 +6,60 @@ class UserRepository extends DatabaseConnection{
 	private static $username = 'username';
 	private static $password = 'password';
 
-	
+	public function __construct(){
+		$this->dbTable = 'login';
+		$this->users = new UserList();
+	}
+
+	//
+	public function query($sql, $params = NULL) {
+		try {
+			$db = $this -> connection();
+
+			$query = $db -> prepare($sql);
+			$result;
+			if ($params != NULL) {
+				if (!is_array($params)) {
+					$params = array($params);
+				}
+
+				$result = $query -> execute($params);
+			} else {
+				$result = $query -> execute();
+			}
+
+			if ($result) {
+				return $result -> fetchAll();
+			}
+
+			return NULL;
+		} catch (\PDOException $e) {
+			die('An unknown error have occured.');
+		}
+	}
+
+	//Funktion för att hämta alla namn ur databasen.
+	public function getAll(){
+		try{
+			$db = $this->connection();
+			$sql = "SELECT * FROM $this->dbTable";
+			$query->execute();
+
+			foreach ($query->fetchAll() as $user) {
+				$userId = $user['userId'];
+				$username = $user['username'];
+				$password = $user['password'];
+
+				$user = new User($userId, $username, $password);
+				$this->users->add($user);
+			}
+			return $this->users;
+		}
+		catch(\PDOException $e){
+			echo "<pre>";
+			var_dump($e);
+			echo "</pre>";
+			die("Error while connection to database.");
+		}
+	}
 }
